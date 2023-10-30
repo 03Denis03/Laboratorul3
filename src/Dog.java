11ba0a617f738da4a79be6903106508d85dd9a96
DenisIonut <cionca.denisionut@gmail.com>
2023-10-30 19:00:55 +0200

    +

---
+++ src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog {
+    public String name;
+    public int age;
+
+    public Dog(String  num, int i) {
+        name = num;
+        age = i;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        Dog dog1 = new Dog("Max", 2);
+        Dog dog2 = new Dog("Maia",4);
+        System.out.println("Name: " + dog1.name + " " + "Age: " + dog1.age);
+        System.out.println("Name: " + dog1.name + " " + "Age: " + dog1.age);
     }
 }
