2e615362f2937c84c929d7963909128412dbcd17
DenisIonut <cionca.denisionut@gmail.com>
2023-10-31 17:39:57 +0200

    +

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Dog dog1 = new Dog("Max", 2);
         Dog dog2 = new Dog("Maia",4);
+        System.out.println("Values of objects before using setter method:");
         System.out.println("Name: " + dog1.name + " " + "Age: " + dog1.age);
+        System.out.println("Name: " + dog2.name + " " + "Age: " + dog2.age);
+        dog1.setNameAndAge("Rex", 5);
+        dog2.setNameAndAge("Linda", 3);
+        System.out.println("Values of objects after using setter method:");
         System.out.println("Name: " + dog1.name + " " + "Age: " + dog1.age);
+        System.out.println("Name: " + dog2.name + " " + "Age: " + dog2.age);
     }
 }
